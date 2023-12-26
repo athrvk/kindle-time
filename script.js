749342eb60db4ec19daa089786d110d0262c4b09
Atharva Kusumbia <45625606+athrvk@users.noreply.github.com>
2023-12-26 14:59:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,30 +8,69 @@ function padClock(p, n) {
   return p + ('0' + n).slice(-2);
 }
 
-function getClock() {
-  const currentTimeStamp = Date.now();
-  d = new Date(currentTimeStamp);
-  return [
-  use24HourClock ? d.getHours() : d.getHours() % 12 || 12,
-  d.getMinutes(),
-  d.getSeconds()].
+async function getCurrentTimezoneByIp() {
+  const api = "https://worldtimeapi.org/api/ip";
+  let timezone;
+  try {
+    const response = await fetch(api);
 
-  reduce(padClock, '');
+    if (!response.ok) {
+      throw new Error(`API request failed with status ${response.status}`);
+    }
+
+    const jsonData = await response.json();
+    timezone = jsonData.timezone;
+  } catch (error) {
+    console.error("Error fetching time:", error);
+  }
+  return timezone;
+}
+
+function changeTimezone(date, ianatz) {
+
+  // suppose the date is 12:00 UTC
+  var invdate = new Date(date.toLocaleString('en-US', {
+    timeZone: ianatz
+  }));
+
+  // then invdate will be 07:00 in Toronto
+  // and the diff is 5 hours
+  var diff = date.getTime() - invdate.getTime();
+
+  // so 12:00 in Toronto is 17:00 UTC
+  return new Date(date.getTime() - diff); // needs to substract
+
+}
+
+
+function getClock(timezone) {
+  d = new Date();
+  d = changeTimezone(d, timezone)
+  return [
+    use24HourClock ? d.getHours() : d.getHours() % 12 || 12,
+    d.getMinutes(),
+    d.getSeconds()].
+
+    reduce(padClock, '');
 }
 
 function getClass(n, i2) {
   return classList.find((class_, classIndex) => Math.abs(n - i2) === classIndex) || '';
 }
 
-let loop = setInterval(() => {
-  c = getClock();
 
-  columns.forEach((ele, i) => {
-    let n = +c[i];
-    let offset = -n * size;
-    ele.style.transform = `translateY(calc(50vh + ${offset}px - ${size / 2}px))`;
-    Array.from(ele.children).forEach((ele2, i2) => {
-      ele2.className = 'num ' + getClass(n, i2);
+let loop = setInterval(() => {
+  getCurrentTimezoneByIp()
+    .then((timezone) => {
+      c = getClock(timezone);
+      columns.forEach((ele, i) => {
+        let n = +c[i];
+        let offset = -n * size;
+        ele.style.transform = `translateY(calc(50vh + ${offset}px - ${size / 2}px))`;
+        Array.from(ele.children).forEach((ele2, i2) => {
+          ele2.className = 'num ' + getClass(n, i2);
+        });
+      });
     });
-  });
+
 }, 200 + Math.E * 10);
